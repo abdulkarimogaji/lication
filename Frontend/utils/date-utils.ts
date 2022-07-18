2f16020c67c4f93f3a26f001ec20bc85f4c21fab
AbdulkarimOgaji <abdulkarimogaji002@gmail.com>
2022-07-18 12:38:12 +0100

    chore: parse rfc3339 date format

--- Frontend/utils/date-utils.ts
+++ Frontend/utils/date-utils.ts
@@ -1,8 +1,11 @@
 export const parseDate = (respDate: string): string => {
+    if (respDate == "empty") return ""
     const date = new Date(new Date(Date.parse(respDate)).toLocaleString())
     const local = new Date(Date.now())
+    const hours = date.getHours() < 10 ? "0" + date.getHours(): date.getHours()
+    const minutes = date.getMinutes() < 10 ? "0" + date.getMinutes(): date.getMinutes()
     if (date.getDay() < local.getDay()) return "Yesterday"
-    if (date.getDay() == local.getDay() && date.getDate() == local.getDate()) return date.getHours() + ":" + date.getMinutes()
+    if (date.getDay() == local.getDay() && date.getDate() == local.getDate()) return hours + ":" + minutes
     return date.toLocaleDateString()
 }
 
